cd995f12353d269e7bf3a4f01f21621f0489c558
LilRinZhang <149049060+LilRinZhang@users.noreply.github.com>
2024-01-10 10:55:57 +0900

    Unit 23 training

--- 0850-sample-props/src/main/java/com/example/sample/service/DiscountServiceImpl.java
+++ 0850-sample-props/src/main/java/com/example/sample/service/DiscountServiceImpl.java
@@ -1,26 +1,31 @@
 package com.example.sample.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DiscountServiceImpl implements DiscountService {
 
-    /** ディスカウント率 */
-    @Value("${discount.rate}")
-    private double discountRate;
+    private final DiscountProperties discountProperties;
 
-    /** ディスカウント上限 */
-    @Value("${discount.max}")
-    private int discountMax;
+    public DiscountServiceImpl(DiscountProperties discountProperties) {
+        this.discountProperties = discountProperties;
+    }
+    // /** ディスカウント率 */
+    // @Value("${discount.rate}")
+    // private double discountRate;
+
+    // /** ディスカウント上限 */
+    // @Value("${discount.max}")
+    // private int discountMax;
 
     @Override
     public int calculateDiscountPrice(int originalPrice) {
-
+        double discountRate = discountProperties.getRate();
+        int discountMax = discountProperties.getMax();
         System.out.println("ディスカウント率:" + discountRate);
         System.out.println("ディスカウント上限:" + discountMax);
 
-        int discount = (int)(originalPrice * discountRate);
+        int discount = (int) (originalPrice * discountRate);
 
         if (discount > discountMax) {
             System.out.println("ディスカウント額が上限値を超えました。discount=" + discount + " max=" + discountMax);
